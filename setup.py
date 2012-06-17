90bd0b3374c25c8322c394f8d5962d4e662944ea
Franklin Kingma <franklin@fourdigits.nl>
2012-06-17 19:55:52 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+# This should be only one line. If it must be multi-line, indent the second
+# line onwards to keep the PKG-INFO file format intact.
+"""
+"""
+
+from setuptools import setup, find_packages
+
+
+version = '0.1.dev0'
+
+setup(
+    name='collective.jquery.backstretch',
+    version=version,
+    description='',
+
+    author='Franklin Kingma <franklin@fourdigits.nl>',
+    author_email='franklin@fourdigits.nl',
+
+    license='GPL',
+    url='https://github.com/kingel/collective.jquery.backstretch/',
+    keywords='plone jquery backstretch',
+    classifiers=[],
+    namespace_packages=['collective', 'collective.jquery'],
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'distribute',
+        ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
